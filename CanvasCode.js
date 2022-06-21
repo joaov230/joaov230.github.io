127b5d6c4acaec4ec7872b4c5340d1926bb1042a
João Vítor F. B <jvbeltrame@inf.ufsm.br>
2022-06-21 17:35:01 -0300

    Update CanvasCode.js

--- CanvasCode.js
+++ CanvasCode.js
@@ -4,10 +4,11 @@ var canvasHeight = canvas.height;
 var ctx = canvas.getContext("2d");
 var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
 
-var points = [];
-var drag = false;
-var dragOnCreate = false;
-var p;
+let points = []; // Array of Anchors
+let drag = false; // signals if the mouse is dragging any point
+let p; // Information about the anchor (and it's angle points) that is moving
+let dragOnCreate = false; // signals if the mouse is dragging the angle points after creating the anchor point
+let closedCurve = false; // signals when the curve is a closed path
 
 
 
@@ -15,20 +16,33 @@ var p;
 
 let pointErased = false;
 let ctrlPressed = false;
+let altPressed = false;
 
 function init() {
   window.addEventListener('keydown', function(e) {
+    if (e.code == "AltLeft") {
+      altPressed = true;
+    }
+
     if (e.code == "ControlLeft") {
       ctrlPressed = true;
     }
 
     if (e.code == "KeyZ" && ctrlPressed == true && pointErased == false) {
+      if (closedCurve) {
+        closedCurve = false;
+        return;
+      }
       points.pop();
       pointErased = true;
     }
     draw();
   }, false);
   window.addEventListener('keyup', function(e) {
+    if (e.code == "AltLeft") {
+      altPressed = false;
+    }
+
     if (e.code == "ControlLeft") {
       ctrlPressed = false;
     }
@@ -42,7 +56,7 @@ function init() {
   ctx.canvas.addEventListener("mousedown", function(e) {
     for (i = 0; i < points.length; i++) {
       colidiu = points[i].colides(e.offsetX, e.offsetY);
-      if (colidiu && ctrlPressed) {
+      if (colidiu && ((colidiu == 'anchorPoint' && ctrlPressed) || (colidiu != 'anchorPoint' && altPressed))) {
         p = {
           key: i,
           value: colidiu,
@@ -56,6 +70,9 @@ function init() {
           } : null
         }
         drag = true;
+        return;
+      } else if (colidiu == 'anchorPoint' && i == 0 && points.length > 1) {
+        closedCurve = true;
         return;
       }
     }
@@ -137,59 +154,121 @@ function draw() {
 function drawBezier() {
   ctx.strokeStyle = '#b3198f';
   if (points.length > 1) {
-    for (i = 0; i < points.length-1; i++) {
-      for (t = 0; t < 1; t+=0.01) {
-        part1 = {
-          x: Math.pow((1-t),3)*points[i].anchorPoint.x,
-          y: Math.pow((1-t),3)*points[i].anchorPoint.y
-        }
-        part2 = {
-          x: 3*Math.pow((1-t),2)*t*points[i].angleRight.x,
-          y: 3*Math.pow((1-t),2)*t*points[i].angleRight.y
-        }
-        part3 = {
-          x: 3*(1-t)*Math.pow(t,2)*points[i+1].angleLeft.x,
-          y: 3*(1-t)*Math.pow(t,2)*points[i+1].angleLeft.y
-        }
-        part4 = {
-          x: Math.pow(t,3)*points[i+1].anchorPoint.x,
-          y: Math.pow(t,3)*points[i+1].anchorPoint.y
-        }
-        point1TempX = part1.x + part2.x + part3.x + part4.x;
-        point1TempY = part1.y + part2.y + part3.y + part4.y;
-  
-        tt = t+0.01
-  
-        part1 = {
-          x: Math.pow((1-tt),3)*points[i].anchorPoint.x,
-          y: Math.pow((1-tt),3)*points[i].anchorPoint.y
-        }
-        part2 = {
-          x: 3*Math.pow((1-tt),2)*tt*points[i].angleRight.x,
-          y: 3*Math.pow((1-tt),2)*tt*points[i].angleRight.y
-        }
-        part3 = {
-          x: 3*(1-tt)*Math.pow(tt,2)*points[i+1].angleLeft.x,
-          y: 3*(1-tt)*Math.pow(tt,2)*points[i+1].angleLeft.y
-        }
-        part4 = {
-          x: Math.pow(tt,3)*points[i+1].anchorPoint.x,
-          y: Math.pow(tt,3)*points[i+1].anchorPoint.y
-        }
-        point2TempX = part1.x + part2.x + part3.x + part4.x;
-        point2TempY = part1.y + part2.y + part3.y + part4.y;
-  
-  
-        ctx.beginPath();
-        ctx.moveTo(point1TempX, point1TempY);
-        ctx.lineTo(point2TempX, point2TempY);
-        ctx.stroke();
-        ctx.closePath();
-  
-        point1TempX = point2TempX;
-        point1TempY = point2TempY;
-      }
+    drawOpenCurveBezier();
+    if (closedCurve) {
+      drawClosedCurveBezier();
     }
+  }
+}
+
+function drawOpenCurveBezier() {
+  for (i = 0; i < points.length-1; i++) {
+    for (t = 0; t < 1; t+=0.01) {
+      part1 = {
+        x: Math.pow((1-t),3)*points[i].anchorPoint.x,
+        y: Math.pow((1-t),3)*points[i].anchorPoint.y
+      }
+      part2 = {
+        x: 3*Math.pow((1-t),2)*t*points[i].angleRight.x,
+        y: 3*Math.pow((1-t),2)*t*points[i].angleRight.y
+      }
+      part3 = {
+        x: 3*(1-t)*Math.pow(t,2)*points[i+1].angleLeft.x,
+        y: 3*(1-t)*Math.pow(t,2)*points[i+1].angleLeft.y
+      }
+      part4 = {
+        x: Math.pow(t,3)*points[i+1].anchorPoint.x,
+        y: Math.pow(t,3)*points[i+1].anchorPoint.y
+      }
+      point1TempX = part1.x + part2.x + part3.x + part4.x;
+      point1TempY = part1.y + part2.y + part3.y + part4.y;
+
+      tt = t+0.01
+
+      part1 = {
+        x: Math.pow((1-tt),3)*points[i].anchorPoint.x,
+        y: Math.pow((1-tt),3)*points[i].anchorPoint.y
+      }
+      part2 = {
+        x: 3*Math.pow((1-tt),2)*tt*points[i].angleRight.x,
+        y: 3*Math.pow((1-tt),2)*tt*points[i].angleRight.y
+      }
+      part3 = {
+        x: 3*(1-tt)*Math.pow(tt,2)*points[i+1].angleLeft.x,
+        y: 3*(1-tt)*Math.pow(tt,2)*points[i+1].angleLeft.y
+      }
+      part4 = {
+        x: Math.pow(tt,3)*points[i+1].anchorPoint.x,
+        y: Math.pow(tt,3)*points[i+1].anchorPoint.y
+      }
+      point2TempX = part1.x + part2.x + part3.x + part4.x;
+      point2TempY = part1.y + part2.y + part3.y + part4.y;
+
+
+      ctx.beginPath();
+      ctx.moveTo(point1TempX, point1TempY);
+      ctx.lineTo(point2TempX, point2TempY);
+      ctx.stroke();
+      ctx.closePath();
+
+      point1TempX = point2TempX;
+      point1TempY = point2TempY;
+    }
+  }
+}
+
+function drawClosedCurveBezier() {
+  i = (points.length-1);
+  for (t = 0; t < 1; t+=0.01) {
+    part1 = {
+      x: Math.pow((1-t),3)*points[i].anchorPoint.x,
+      y: Math.pow((1-t),3)*points[i].anchorPoint.y
+    }
+    part2 = {
+      x: 3*Math.pow((1-t),2)*t*points[i].angleRight.x,
+      y: 3*Math.pow((1-t),2)*t*points[i].angleRight.y
+    }
+    part3 = {
+      x: 3*(1-t)*Math.pow(t,2)*points[0].angleLeft.x,
+      y: 3*(1-t)*Math.pow(t,2)*points[0].angleLeft.y
+    }
+    part4 = {
+      x: Math.pow(t,3)*points[0].anchorPoint.x,
+      y: Math.pow(t,3)*points[0].anchorPoint.y
+    }
+    point1TempX = part1.x + part2.x + part3.x + part4.x;
+    point1TempY = part1.y + part2.y + part3.y + part4.y;
+
+    tt = t+0.01
+
+    part1 = {
+      x: Math.pow((1-tt),3)*points[i].anchorPoint.x,
+      y: Math.pow((1-tt),3)*points[i].anchorPoint.y
+    }
+    part2 = {
+      x: 3*Math.pow((1-tt),2)*tt*points[i].angleRight.x,
+      y: 3*Math.pow((1-tt),2)*tt*points[i].angleRight.y
+    }
+    part3 = {
+      x: 3*(1-tt)*Math.pow(tt,2)*points[0].angleLeft.x,
+      y: 3*(1-tt)*Math.pow(tt,2)*points[0].angleLeft.y
+    }
+    part4 = {
+      x: Math.pow(tt,3)*points[0].anchorPoint.x,
+      y: Math.pow(tt,3)*points[0].anchorPoint.y
+    }
+    point2TempX = part1.x + part2.x + part3.x + part4.x;
+    point2TempY = part1.y + part2.y + part3.y + part4.y;
+
+
+    ctx.beginPath();
+    ctx.moveTo(point1TempX, point1TempY);
+    ctx.lineTo(point2TempX, point2TempY);
+    ctx.stroke();
+    ctx.closePath();
+
+    point1TempX = point2TempX;
+    point1TempY = point2TempY;
   }
 }
 
