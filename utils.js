0b3fd3b6a9a4fc5b595cdcd2653a9748efa69c51
João Vítor F. B <jvbeltrame@inf.ufsm.br>
2023-11-07 14:28:24 -0300

    baby wake up, new version just dropped

--- utils.js
+++ utils.js
@@ -6,6 +6,26 @@ function isNullString(value) {
     }
 }
 
+function checkIfSolutionIsCorrect(fb) {
+    if (Array.isArray(fb)) {
+        if (fb.length > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (fb.hasOwnProperty("feedback")) {
+        if (fb.success) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (fb.success) {
+            return true;
+        }
+    }
+}
+
 function getRandExURL(userId = null, dificuldade = 0) {
     var url = "https://script.google.com/macros/s/AKfycbwZKg-bWoZs_OgVkRUmvxxfrdQeSTWbk3lANkRDUPik-zAvLWfieRkhCgFrU415LYYg/exec?actionRequest=getExercicioAleatorio";
 
@@ -39,10 +59,37 @@ function getCookie(cName) {
     cArr.forEach(val => {
         if (val.indexOf(name) === 0) res = val.substring(name.length);
     });
-    res = JSON.parse(res);
+
+    if (typeof res == "undefined") {
+        res = null;
+    } else {
+        res = JSON.parse(res);
+    }
     return res;
 }
 
+/*
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+*/
 function setUserLevel(userLevelInfo) {
     const progressBarPercentRate = 10;
 
@@ -67,4 +114,26 @@ function btnDisable(btnName) {
     $(btnName).prop('disabled', true);
     $(btnName).css('background-color', '#a5a5a5');
     $(btnName).css('cursor', 'wait');
+}
+
+function del_cookie(name) {
+    document.cookie = name + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
+}
+
+function del_cookies() {
+    del_cookie('userData');
+    del_cookie('auth');
+}
+
+function logout(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    del_cookies();
+    window.location.href = './index.html';
+}
+
+function accessDenied_returningToMain() {
+    del_cookies();
+    window.location.href = './index.html';
 }
